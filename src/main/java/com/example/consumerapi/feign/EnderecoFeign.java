0f0f9c5cb2f88f9ba0cb1375f3294ab05745585b
MatheusHBMelo <matheushbmelo@gmail.com>
2023-11-27 20:10:19 -0300

    Fix: Bug fixes

--- src/main/java/com/example/consumerapi/feign/EnderecoFeign.java
+++ src/main/java/com/example/consumerapi/feign/EnderecoFeign.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "viacep.com.br/ws/", name = "viacep")
+@FeignClient(url = "https://viacep.com.br/ws/", name = "viacep")
 public interface EnderecoFeign {
     @GetMapping("{cep}/json")
     EnderecoResponse buscarCep(@PathVariable(value = "cep") String cep);
--- src/main/java/com/example/consumerapi/models/EnderecoRequest.java
+++ src/main/java/com/example/consumerapi/models/EnderecoRequest.java
@@ -3,7 +3,6 @@ package com.example.consumerapi.models;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class EnderecoRequest {
     private String cep;
