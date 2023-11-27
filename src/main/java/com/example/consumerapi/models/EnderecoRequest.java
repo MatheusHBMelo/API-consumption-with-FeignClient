033da91681ac567954b5aade5fc28741dac7fe79
MatheusHBMelo <matheushbmelo@gmail.com>
2023-11-27 19:53:03 -0300

    Feat: final EnderecoRequest class

---
+++ src/main/java/com/example/consumerapi/models/EnderecoRequest.java
@@ -0,0 +1,10 @@
+package com.example.consumerapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class EnderecoRequest {
+    private String cep;
+}
